f4c10ea9d1e6845b12cea5a48c9801639b9cfd9c
Ivana Piljic <piljic.ivana@gmail.com>
2020-06-24 18:21:54 +0100

    adding buttons to welcome screen

--- App.js
+++ App.js
@@ -3,20 +3,10 @@ import { View, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import AppButton from "./app/components/AppButton";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
-// import WelcomeScreen from "./app/screens/WelcomeScreen";
 // import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <AppButton title="Login" onPress={() => console.log("Tapp")} />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
