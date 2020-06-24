e40a9f44ab531fb2b5622220d2ee36c2fe3be91a
Ivana Piljic <piljic.ivana@gmail.com>
2020-06-24 12:31:20 +0100

    making a subfolder for app colors

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 
+import colors from "../config/colors";
+
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
@@ -19,7 +21,7 @@ const styles = StyleSheet.create({
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#fc5c65",
+    backgroundColor: colors.primary,
     position: "absolute",
     top: 20,
     left: 30,
@@ -31,7 +33,7 @@ const styles = StyleSheet.create({
   deleteIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#fecdc4",
+    backgroundColor: colors.secondary,
     position: "absolute",
     top: 20,
     right: 30,
