f2a3f4f0b5811fcc0e54f9dcb36e13effc015e99
Ivana Piljic <piljic.ivana@gmail.com>
2020-06-25 15:34:33 +0100

    styling close, delete button in view image screen

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { View } from "react-native";
 
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+// import Card from "./app/components/Card";
+// import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 // import AppButton from "./app/components/AppButton";
 // import WelcomeScreen from "./app/screens/WelcomeScreen";
-// import ViewImageScreen from "./app/screens/ViewImageScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
